0065b254426eab8f015a1ae93f7b029c9f0dd72b
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-04-08 15:47:21 +0430

    Clean-up

--- src/Presentations/WebApi/Areas/Manage/Models/RoleDto.cs
+++ src/Presentations/WebApi/Areas/Manage/Models/RoleDto.cs
@@ -1,10 +1,12 @@
-﻿namespace WebApi.Areas.Manage.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Areas.Manage.Models
 {
     public class CreateRoleMDto
     {
         [Required(ErrorMessage = "وارد کردن نام نقش ضروری است.")]
-        public string name { get; set; }
-        public string title { get; set; }
-        public string description { get; set; }
+        public string Name { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
     }
 }
