cdf7fcb7881878a136a9ffefab4e1218bc1e4605
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-04-16 23:45:07 +0430

    Location Entity

---
+++ src/Domain/Entities/Location.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    public class Location
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public LocationType Type { get; set; }
+
+        [ForeignKey("Parent")]
+        public long ParentId { get; set; }
+        public virtual Location Parent { get; set; }
+    }
+}
---
+++ src/Domain/Enums/LocationType.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Enums
+{
+    public enum LocationType
+    {
+        Province,
+        City,
+        Region
+    }
+}
