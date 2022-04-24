f7f336b41c345cabc575f23c3f1d7d251c83c60c
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-04-25 00:48:37 +0430

    create region validation

--- src/Presentations/WebApi/Areas/Manage/Models/RegionDto.cs
+++ src/Presentations/WebApi/Areas/Manage/Models/RegionDto.cs
@@ -5,6 +5,7 @@ namespace WebApi.Areas.Manage.Models
     public class CreateRegionDto
     {
         [Required(ErrorMessage = "وارد کردن نام ضروری است.")]
+        [StringLength(20, ErrorMessage = "حداقل 3 و حداکثر 20 کارکتر وارد کنید.", MinimumLength = 3)]
         public string Name { get; set; }
         public long? ParentId { get; set; }
     }
