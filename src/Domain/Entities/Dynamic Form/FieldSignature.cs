ae9ca0cd4334918755fd52c1c19dee7629007166
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-04-28 01:17:55 +0430

    Dynamic form - cleanup.

--- src/Domain/Entities/Dynamic Form/FieldSignature.cs
+++ src/Domain/Entities/Dynamic Form/FieldSignature.cs
@@ -1,9 +1,11 @@
 ﻿using Domain.Enums.Dynamic_Form;
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Entities.Dynamic_Form
 {
     public class FieldSignature
     {
+        [Key]
         public string Id { get; set; }
         public string Name { get; set; }
         public string RegEx { get; set; }
