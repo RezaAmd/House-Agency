755b6a578ac202b9b50cd27f39699e808ef6ff1e
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-04-29 03:21:54 +0430

    Create a pagination parameter.

---
+++ src/Application/Models/PaginationParameter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models
+{
+    public class PaginationParameter
+    {
+        [FromQuery]
+        [Range(1, 1000, ErrorMessage = "حداقل شماره صفحه 1 و حداکثر 1000 می باشد.")]
+        public int Page { get; set; } = 1;
+    }
+}
