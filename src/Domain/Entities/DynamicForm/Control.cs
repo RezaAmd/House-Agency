ff8a5cdcff6c265e118162379f97171fa653dbaf
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-04-29 01:10:29 +0430

    cleanup

--- src/Domain/Entities/DynamicForm/Control.cs
+++ src/Domain/Entities/DynamicForm/Control.cs
@@ -11,7 +11,7 @@ namespace Domain.Entities
         public string Label { get; set; }
         public string? Placeholder { get; set; }
         public FieldType Type { get; set; }
-        public int DisplayOrder { get; set; }
+        public int Priority { get; set; }
         public long Min { get; set; } // Minimum length - Minimum number.
         public long Max { get; set; } // Maximum length - Maximum number.
         public string? JsonOption { get; set; }
