4055fd4df3a42acf78f6994aa95b75efe02d89ee
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-04-28 22:56:34 +0430

    Possession DAO

---
+++ src/Application/Dao/Possession/IPossessionDao.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace Application.Dao
+{
+    public interface IPossessionDao : IBaseDao<Possession, string>
+    {
+    }
+}
---
+++ src/Application/Dao/Possession/PossessionDao.cs
@@ -0,0 +1,19 @@
+﻿using Application.Interfaces.Context;
+using Domain.Entities;
+
+namespace Application.Dao
+{
+    public class PossessionDao : BaseDao<Possession, string>
+    {
+        #region Dependency Injection
+        private readonly IDbContext context;
+
+        public PossessionDao(IDbContext _context) : base(_context)
+        {
+            context = _context;
+        }
+        #endregion
+
+
+    }
+}
