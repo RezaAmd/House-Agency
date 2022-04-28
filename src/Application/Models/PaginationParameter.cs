9370db24835e8469012fe98f19c493f0e41e29fa
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-04-29 03:52:31 +0430

    Pagination parameters with page size.

--- src/Application/Models/PaginationParameter.cs
+++ src/Application/Models/PaginationParameter.cs
@@ -7,4 +7,10 @@ namespace Application.Models
         [Range(1, 1000, ErrorMessage = "حداقل شماره صفحه 1 و حداکثر 1000 می باشد.")]
         public int Page { get; set; } = 1;
     }
+
+    public class PaginationWithSizeParameter : PaginationParameter
+    {
+        [Range(1, 100, ErrorMessage = "حداقل شماره صفحه 1 و حداکثر 1000 می باشد.")]
+        public int PageSize { get; set; } = 20;
+    }
 }
