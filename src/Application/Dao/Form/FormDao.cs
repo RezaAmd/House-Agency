2009d15ee47503c3da9f51e6b23a722e0e845fd6
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-04-29 01:10:59 +0430

    cleanup

--- src/Application/Dao/Form/FormDao.cs
+++ src/Application/Dao/Form/FormDao.cs
@@ -3,7 +3,7 @@ using Domain.Entities;
 
 namespace Application.Dao
 {
-    public class FormDao : BaseDao<Form, long>, IFormDao
+    public class FormDao : BaseDao<Form, string>, IFormDao
     {
         #region Dependency Injection
         private readonly IDbContext context;
