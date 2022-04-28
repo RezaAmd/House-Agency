bf19403a5c55a2967343a3087ab0c160eabe81eb
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-04-28 22:55:59 +0430

    Control DAO

---
+++ src/Application/Dao/Control/ControlDao.cs
@@ -0,0 +1,19 @@
+﻿using Application.Interfaces.Context;
+using Domain.Entities;
+
+namespace Application.Dao
+{
+    public class ControlDao : BaseDao<Control, long>
+    {
+        #region Dependency Injection
+        private readonly IDbContext context;
+
+        public ControlDao(IDbContext _context) : base(_context)
+        {
+            context = _context;
+        }
+        #endregion
+
+
+    }
+}
---
+++ src/Application/Dao/Control/IControlDao.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Application.Dao
+{
+    internal interface IControlDao : IBaseDao<Control, string>
+    {
+
+    }
+}
