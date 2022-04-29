146888528118cd9430cc73705862d88233643865
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-04-29 16:46:19 +0430

    Add id to FormControl.

--- src/Domain/Entities/DynamicForm/FormControl.cs
+++ src/Domain/Entities/DynamicForm/FormControl.cs
@@ -4,19 +4,21 @@ namespace Domain.Entities
 {
     public class FormControl : BaseEntity
     {
+#nullable disable
         #region Constructors
         FormControl() { }
         public FormControl(string formId, string controlId, int priority = 0)
         {
+            Id = Guid.NewGuid().ToString();
             Priority = priority;
             FormId = formId;
             ControlId = controlId;
         }
         #endregion
 
+        public string Id { get; set; }
         public int Priority { get; set; } // order by this for priority.
 
-#nullable disable
         [ForeignKey("Form")]
         public string FormId { get; set; }
         public Form Form { get; set; }
@@ -24,6 +26,5 @@ namespace Domain.Entities
         [ForeignKey("Control")]
         public string ControlId { get; set; }
         public Control Control { get; set; }
-#nullable enable
     }
 }
