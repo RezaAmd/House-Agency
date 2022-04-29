e4690fe79e345584b5bbe51d5bf7005fccf2e38d
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-04-29 12:10:57 +0430

    cleanup

--- tests/XUnit/WebApi.Test/Manage/UserControllerTest.cs
+++ tests/XUnit/WebApi.Test/Manage/UserControllerTest.cs
@@ -1,10 +1,4 @@
-﻿using Application.Dao;
-using Application.Interfaces.Context;
-using Application.Models;
-using Microsoft.Extensions.Logging;
-using System.Net.Http;
-using System.Threading.Tasks;
-using WebApi.Areas.Manage.Controllers;
+﻿using System.Threading.Tasks;
 using Xunit;
 
 namespace WebApi.Test.Manage
