6749593374a66b3a172f044091237474b89d469c
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-04-29 12:59:15 +0430

    Form fluent api.

---
+++ src/Infrastructure/Persistence/Configurations/FluentApi/Form/FormConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations.FluentApi
+{
+    public class FormConfiguration : IEntityTypeConfiguration<Form>
+    {
+        public void Configure(EntityTypeBuilder<Form> b)
+        {
+            b.ToTable("Forms");
+
+            b.HasIndex(e => e.Name)
+                .IsUnique();
+
+            b.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            b.Property(e => e.Title)
+                .HasMaxLength(30);
+
+            // Each User can have many entries in the UserRole join table
+            b.HasMany(e => e.FormControls)
+                .WithOne(e => e.Form);
+        }
+    }
+}
