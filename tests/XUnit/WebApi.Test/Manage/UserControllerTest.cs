a85c26d77387ac4765c95500708a1c5c440525b2
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-04-29 11:51:06 +0430

    XUnit Test

---
+++ tests/XUnit/WebApi.Test/Manage/UserControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using Application.Dao;
+using Application.Interfaces.Context;
+using Application.Models;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApi.Areas.Manage.Controllers;
+using Xunit;
+
+namespace WebApi.Test.Manage
+{
+    public class UserControllerTest
+    {
+        public UserControllerTest()
+        {
+        }
+
+
+        [Fact]
+        public async Task GetAllTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ tests/XUnit/WebApi.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace WebApi.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
