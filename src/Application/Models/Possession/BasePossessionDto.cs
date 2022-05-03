189eaf3e0e3c0cd603dc51e4e28e691808f59ef5
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-05-03 15:45:54 +0430

    Possession feature

---
+++ src/Application/Models/Possession/BasePossessionDto.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models.Possession
+{
+    public class BasePossessionDto
+    {
+        public string title { get; set; }
+        public string? description { get; set; }
+        public int meter { get; set; }
+        public DateTime? ConstructionDateTime { get; set; } //
+        public PossessionType Type { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public PossessionState State { get; set; }
+    }
+
+    public class PossessionDto
+    {
+        public BasePossessionDto Base { get; set; }
+        public Dictionary<string,object> DynamicForm { get; set; }
+    }
+}
---
+++ src/Presentations/WebApi/Areas/Manage/Controllers/PossessionController.cs
@@ -0,0 +1,21 @@
+﻿using Application.Models;
+using Application.Models.Possession;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Areas.Manage.Controllers
+{
+    [ApiController]
+    [Area("manage")]
+    [Route("[area]/[controller]/[action]")]
+    public class PossessionController : ControllerBase
+    {
+
+        [HttpPost]
+        public ApiResult<object> Entrust([FromBody] PossessionDto model)
+        {
+
+            return Ok(model);
+        }
+    }
+}
