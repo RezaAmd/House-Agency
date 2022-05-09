c79f47822a772143cf963cfa9fd61ac9a008bf46
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-05-10 01:00:18 +0430

    Add constructors

--- src/Application/Models/SelectOption.cs
+++ src/Application/Models/SelectOption.cs
@@ -2,9 +2,22 @@
 {
     public class SelectItem
     {
+        #region Constructors
+        public SelectItem() { }
+        public SelectItem(string text, string value, bool isSelected = false, bool isDisabled = false)
+        {
+            Text = text;
+            Value = value;
+            IsSelected = isSelected;
+            IsDisabled = isDisabled;
+            Children = new List<SelectItem>();
+        }
+        #endregion
+
         public string Text { get; set; }
         public string Value { get; set; }
         public bool IsSelected { get; set; }
         public bool IsDisabled { get; set; }
+        public List<SelectItem> Children { get; set; }
     }
 }
