52de9c4add154d8bafc3870caf9f32ef1a683c6b
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-05-10 01:00:27 +0430

    update service

--- src/Application/Models/Dto/Possession/BasePossessionDto.cs
+++ src/Application/Models/Dto/Possession/BasePossessionDto.cs
@@ -1,17 +1,26 @@
 ﻿using Domain.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Application.Models.Dto
 {
     public class BasePossessionDto
     {
-        public string title { get; set; }
-        public string? description { get; set; }
+        [Required(ErrorMessage = "محدوده آگهی را مشخص کنید.")]
+        public int RegionId { get; set; }
+        [Required(ErrorMessage = "متراژ سازه در آگهی ضروری است.")]
         public int meter { get; set; }
-        public string ConstructionDate { get; set; } //
-        public long RegionId { get; set; }
+        #region prices
+        public ulong Price { get; set; }
+        public ulong Rent { get; set; }
+        public ulong Mortgage { get; set; }
+        #endregion
+        public string? ConstructionDate { get; set; } // YYYY
         public PossessionType Type { get; set; }
         public TransactionType TransactionType { get; set; }
-        public PossessionState State { get; set; }
+        public bool IsDraft { get; set; }
+        [Required(ErrorMessage = "عنوان آگهی ضروری است.")]
+        public string title { get; set; }
+        public string? description { get; set; }
     }
 
     public class PossessionDto
