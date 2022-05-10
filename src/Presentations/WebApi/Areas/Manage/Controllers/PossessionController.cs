5a9a8c21c8be3cbcdc9e36f609a51a6235035f01
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-05-10 20:24:55 +0430

    Cleanup get possession types.

--- src/Presentations/WebApi/Areas/Manage/Controllers/PossessionController.cs
+++ src/Presentations/WebApi/Areas/Manage/Controllers/PossessionController.cs
@@ -4,8 +4,6 @@ using Application.Models.Dto;
 using Application.Services;
 using Domain.Entities;
 using Domain.Enums;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
@@ -31,108 +29,109 @@ namespace WebApi.Areas.Manage.Controllers
         {
             var resultList = new List<SelectItem>();
 
-            var maskooni = new SelectItem("مسکونی", "1", true);
-            maskooni.Children.Add(new SelectItem("ویلایی/باغ و باغچه", "1", true));
-            maskooni.Children.Add(new SelectItem("آپارتمان/برج", "2"));
-            maskooni.Children.Add(new SelectItem("مستغلات", "3"));
-            maskooni.Children.Add(new SelectItem("زمین/کلنگی", "4"));
-            maskooni.Children.Add(new SelectItem("پنت هاوس", "5"));
+            var maskooni = new SelectItem(PossessionApplicationType.Residential, true);
+            maskooni.Children = new List<SelectItem>();
+            maskooni.Children.Add(new SelectItem(PossessionType.Vila_Garden, true));
+            maskooni.Children.Add(new SelectItem(PossessionType.Apartment_Tower));
+            maskooni.Children.Add(new SelectItem(PossessionType.RealEstate));
+            maskooni.Children.Add(new SelectItem(PossessionType.Land_OldHouse));
+            maskooni.Children.Add(new SelectItem(PossessionType.Penthouse));
             resultList.Add(maskooni);
 
-            var edari = new SelectItem("اداری/تجاری", "2");
-            edari.Children.Add(new SelectItem("دفتر کار/اتاق اداری و مطب", "1", true));
-            edari.Children.Add(new SelectItem("انبار/سوله/کارگاه و کارخانه", "2"));
-            edari.Children.Add(new SelectItem("کشاورزی", "3"));
-            edari.Children.Add(new SelectItem("مستغلات", "4"));
-            edari.Children.Add(new SelectItem("زمین/کلنگی", "5"));
+            var edari = new SelectItem(PossessionApplicationType.CommercialOffice);
+            edari.Children = new List<SelectItem>();
+            edari.Children.Add(new SelectItem(PossessionType.Vila_Garden, true));
+            edari.Children.Add(new SelectItem(PossessionType.Warehouse_Factory_Workshop));
+            edari.Children.Add(new SelectItem(PossessionType.Agriculture));
+            edari.Children.Add(new SelectItem(PossessionType.RealEstate));
+            edari.Children.Add(new SelectItem(PossessionType.Land_OldHouse));
             resultList.Add(edari);
-
 
             return Ok(resultList);
         }
 
-        [HttpGet]
-        public async Task<ApiResult<object>> GetPossessionType(PossessionType type)
-        {
-            var selectTypeList = new List<SelectItem>();
-            if (type == PossessionType.Residential)
-            {
-                selectTypeList.Add(new SelectItem()
-                {
-                    Text = "ویلایی/باغ و باغچه",
-                    Value = "1",
-                    IsDisabled = false,
-                    IsSelected = true
-                });
-                selectTypeList.Add(new SelectItem()
-                {
-                    Text = "آپارتمان/برج",
-                    Value = "2",
-                    IsDisabled = false,
-                    IsSelected = false
-                });
-                selectTypeList.Add(new SelectItem()
-                {
-                    Text = "مستغلات",
-                    Value = "3",
-                    IsDisabled = false,
-                    IsSelected = false
-                });
-                selectTypeList.Add(new SelectItem()
-                {
-                    Text = "زمین/کلنگی",
-                    Value = "4",
-                    IsDisabled = false,
-                    IsSelected = false
-                });
-                selectTypeList.Add(new SelectItem()
-                {
-                    Text = "پنت هاوس",
-                    Value = "5",
-                    IsDisabled = false,
-                    IsSelected = false
-                });
-            }
-            else if(type == PossessionType.CommercialOffice)
-            {
-                selectTypeList.Add(new SelectItem()
-                {
-                    Text = "دفتر کار/اتاق اداری و مطب",
-                    Value = "1",
-                    IsDisabled = false,
-                    IsSelected = true
-                });
-                selectTypeList.Add(new SelectItem()
-                {
-                    Text = "انبار/سوله/کارگاه و کارخانه",
-                    Value = "2",
-                    IsDisabled = false,
-                    IsSelected = false
-                });
-                selectTypeList.Add(new SelectItem()
-                {
-                    Text = "کشاورزی",
-                    Value = "3",
-                    IsDisabled = false,
-                    IsSelected = false
-                });
-                selectTypeList.Add(new SelectItem()
-                {
-                    Text = "مستغلات",
-                    Value = "4",
-                    IsDisabled = false,
-                    IsSelected = false
-                });
-                selectTypeList.Add(new SelectItem()
-                {
-                    Text = "زمین/کلنگی",
-                    Value = "5",
-                    IsDisabled = false,
-                    IsSelected = false
-                });
-            }
-            return Ok(selectTypeList);
-        }
+        //[HttpGet]
+        //public async Task<ApiResult<object>> GetPossessionType(PossessionType type)
+        //{
+        //    var selectTypeList = new List<SelectItem>();
+        //    if (type == PossessionType.Residential)
+        //    {
+        //        selectTypeList.Add(new SelectItem()
+        //        {
+        //            Text = "ویلایی/باغ و باغچه",
+        //            Value = "1",
+        //            IsDisabled = false,
+        //            IsSelected = true
+        //        });
+        //        selectTypeList.Add(new SelectItem()
+        //        {
+        //            Text = "آپارتمان/برج",
+        //            Value = "2",
+        //            IsDisabled = false,
+        //            IsSelected = false
+        //        });
+        //        selectTypeList.Add(new SelectItem()
+        //        {
+        //            Text = "مستغلات",
+        //            Value = "3",
+        //            IsDisabled = false,
+        //            IsSelected = false
+        //        });
+        //        selectTypeList.Add(new SelectItem()
+        //        {
+        //            Text = "زمین/کلنگی",
+        //            Value = "4",
+        //            IsDisabled = false,
+        //            IsSelected = false
+        //        });
+        //        selectTypeList.Add(new SelectItem()
+        //        {
+        //            Text = "پنت هاوس",
+        //            Value = "5",
+        //            IsDisabled = false,
+        //            IsSelected = false
+        //        });
+        //    }
+        //    else if(type == PossessionType.CommercialOffice)
+        //    {
+        //        selectTypeList.Add(new SelectItem()
+        //        {
+        //            Text = "دفتر کار/اتاق اداری و مطب",
+        //            Value = "1",
+        //            IsDisabled = false,
+        //            IsSelected = true
+        //        });
+        //        selectTypeList.Add(new SelectItem()
+        //        {
+        //            Text = "انبار/سوله/کارگاه و کارخانه",
+        //            Value = "2",
+        //            IsDisabled = false,
+        //            IsSelected = false
+        //        });
+        //        selectTypeList.Add(new SelectItem()
+        //        {
+        //            Text = "کشاورزی",
+        //            Value = "3",
+        //            IsDisabled = false,
+        //            IsSelected = false
+        //        });
+        //        selectTypeList.Add(new SelectItem()
+        //        {
+        //            Text = "مستغلات",
+        //            Value = "4",
+        //            IsDisabled = false,
+        //            IsSelected = false
+        //        });
+        //        selectTypeList.Add(new SelectItem()
+        //        {
+        //            Text = "زمین/کلنگی",
+        //            Value = "5",
+        //            IsDisabled = false,
+        //            IsSelected = false
+        //        });
+        //    }
+        //    return Ok(selectTypeList);
+        //}
 
         [HttpPost]
         [ModelStateValidator]
@@ -141,8 +140,10 @@ namespace WebApi.Areas.Manage.Controllers
             string? userId = User.GetCurrentUserId();
             if (string.IsNullOrEmpty(userId))
             {
-                var newPossession = new Possession(model.Base.title, model.Base.meter, model.Base.RegionId, model.Base.Type, model.Base.TransactionType,
+                var newPossession = new Possession(model.Base.title, model.Base.meter, model.Base.RegionId,
+                    model.Base.Type, model.Base.ApplicationType, model.Base.TransactionType,
                     "c46c9f0f-cabf-47c4-ba70-505085f386bd", DateTime.Now.AddYears(-5));
+
                 var createNewPossessionResult = await possessionService.CreateAsync(newPossession);
                 if (createNewPossessionResult.Succeeded)
                 {
